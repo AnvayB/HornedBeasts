77536e6c597c6eb14054416d2ef9404cd5adc716
Anvay Bhanap <anvay.bhanap@gmail.com>
2021-07-17 18:10:11 -0700

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import { defaultModifiers } from '@popperjs/core/lib/popper-lite';
+import React from 'react';
+
+class Footer extends React.Component {
+  render () {
+    return (
+      <div>
+        <h2>
+          Author: Anvay Bhanap
+        </h2>
+      </div>
+    )
+  }
+}
+
+export default Footer;
