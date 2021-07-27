199ee6b972bcbb13c6c1561fda5b284f6251869c
Anvay Bhanap <anvay.bhanap@gmail.com>
2021-07-26 17:59:43 -0700

    lab 3 complete?

--- src/App.js
+++ src/App.js
@@ -2,15 +2,42 @@ import React from 'react';
 import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
+import data from './data.json';
+import SelectedBeasts from './SelectedBeasts.js';
+
 
 class App extends React.Component {
-  render () {
-    return (
-      <div className = "app">
-        <Header />
-        <Main />
-        <Footer />
+  constructor(props) {
+    super(props);
+    this.state = {
+      beasts: data,
+      modal: false,
+      favoritedBeast: {}
+    };
+  }
 
+  showModal = (name) => {
+    let favoritedBeast = data.find(beast => beast.title === name);
+    this.setState({favoritedBeast, modal: true });
+  }
+  hideModal = () => {
+    this.setState({modal: false});
+  }
+
+
+  render() {
+    return (
+      <div className = "App">
+        <Header />
+        <Main
+          beasts = {this.state.beasts}
+          showModal = {this.showModal}/>
+        <SelectedBeasts
+          favoritedBeast = {this.state.favoritedBeast}
+          show = {this.state.modal}
+          closeModal = {this.hideModal}
+        />
+        <Footer />
       </div>
     );
   }
--- src/Main.js
+++ src/Main.js
@@ -1,30 +1,24 @@
 import React from 'react';
-import data from './data.json';
 import HornedBeasts from './HornedBeasts.js';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CardColumns from 'react-bootstrap/CardColumns';
 
 class Main extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: data
-    };
-  }
-
-  allBeasts = (data) => {
-    let array = data.map((value) => {
-      return <HornedBeasts title={value.title} image_url={value.image_url} description = {value.description} horns = {value.horns}/>;
-    });
-    return array;
-  }
-
-
-
-  render () {
+  render() {
     return (
-      <div className = "main">
-        {this.allBeasts(data)}
+      <div id="beastCards">
+        <CardColumns>
+          {this.props.beasts.map((beast, index) =>
+            <HornedBeasts
+              title={beast.title}
+              image={beast.image_url}
+              description={beast.description}
+              horns={beast.horns}
+              key={index}
+              showModal={this.props.showModal}
+            />
+          )};
+        </CardColumns>
       </div>
     );
   }
